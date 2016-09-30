ebd842a9e3c63858836b1bbf6211d3ea74a06b08
Vitalijus <vitalij.desuk@gmail.com>
2016-09-30 15:58:25 +0100

    method all_counties and by_counties added

--- charge_points_uk/charge_points.rb
+++ charge_points_uk/charge_points.rb
@@ -6,11 +6,22 @@ class ChargePoints
     format :json
 
     def self.all_counties
-        response = get("http://chargepints.dft.gov.uk/api/retrieve/registry/format/json")
-        response['ChargeDevice']
+    	response = get("http://chargepoints.dft.gov.uk/api/retrieve/registry/format/json")
+        response['ChargeDevice'].each do |charger|
+        	puts charger['ChargeDeviceLocation']['Address']['County']
+        end
+    end
+
+    def self.by_counties(county)
+    	response = get("http://chargepoints.dft.gov.uk/api/retrieve/registry/format/json")
+    	response['ChargeDevice'].each do |charger|
+        	if charger['ChargeDeviceLocation']['Address']['County'] == county
+        		puts county
+        	end
+        end
     end
 
 end
 
-charge_points = ChargePoints.new
-puts charge_points.all_counties
+ChargePoints.all_counties
+ChargePoints.by_counties("Greater London")
