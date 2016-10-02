f51adddead1f3c38374a1aadc0283241f57d6863
Vitalijus <vitalij.desuk@gmail.com>
2016-10-02 16:09:21 +0100

    add banking simulator

---
+++ report/email_report.rb
@@ -0,0 +1,20 @@
+require "date"
+
+class EmailReport < Report # EmailReport is a sub-class of BankAccount Report
+    def email_header
+        "Dear Customer,\n\n" +
+        "This report shows your activity as of #{Date.today}"
+    end
+
+    def email_banner
+        "\n\n...........................................................\n\n"
+    end
+
+    def formatted_output
+        email_header + email_banner + super + email_banner + email_footer
+    end
+
+    def email_footer
+        "\nThank you for being with us"
+    end
+end
