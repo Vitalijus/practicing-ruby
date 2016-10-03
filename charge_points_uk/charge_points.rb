216d619580a9ffa9604028ff8d020bff42c76c37
Vitalijus <vitalij.desuk@gmail.com>
2016-10-03 17:58:26 +0100

    Added JSON file, jsonpath and few methods, removed HTTParty

--- charge_points_uk/charge_points.rb
+++ charge_points_uk/charge_points.rb
@@ -1,27 +1,29 @@
 require "json"
-require "httparty"
+require 'jsonpath'
 
-class ChargePoints
-    include HTTParty
-    format :json
+
+class ChargePoints   
+	charge_points = File.read('charge_points.json')
+	RESPONSE = JSON.parse(charge_points)
 
     def self.all_counties
-    	response = get("http://chargepoints.dft.gov.uk/api/retrieve/registry/format/json")
-        response['ChargeDevice'].each do |charger|
-        	puts charger['ChargeDeviceLocation']['Address']['County']
-        end
+        counties = JsonPath.new('$..County').on(RESPONSE.to_json)
+        puts "All counties with charge points for electric car: \n #{counties.compact.uniq}"
     end
 
-    def self.by_counties(county)
-    	response = get("http://chargepoints.dft.gov.uk/api/retrieve/registry/format/json")
-    	response['ChargeDevice'].each do |charger|
-        	if charger['ChargeDeviceLocation']['Address']['County'] == county
-        		puts county
-        	end
-        end
+    def self.by_county(county)
+    	counties = JsonPath.new('$..County').on(RESPONSE.to_json)
+    	counties = counties.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
+    	puts "Number of charge points in #{county}: #{counties[county]}"
+    end
+
+    def self.list_of_streets
+        streets = JsonPath.new('$..Street').on(RESPONSE.to_json)
+        puts "List of streets with charge points for electric car: \n #{streets.compact.uniq - [""]}"
     end
 
 end
 
 ChargePoints.all_counties
-ChargePoints.by_counties("Greater London")
+ChargePoints.by_county("Greater London")
+ChargePoints.list_of_streets
